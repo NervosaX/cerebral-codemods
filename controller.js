575d219a9a8bfea94308f3634daafddfc4ba7aaa
Adam Hoddinott <adam.hoddinott@salespreso.com>
2016-10-30 18:08:38 +1100

    initial commit

---
+++ controller.js
@@ -0,0 +1,21 @@
+module.exports = function(fileInfo, api) {
+  const j = api.jscodeshift;
+  const root =  j(fileInfo.source);
+
+  const modelImports = root.find(j.ImportDeclaration, {
+    source: {
+      value: "cerebral/models/immutable"
+    }
+  });
+
+  const modelImportName = modelImports.find(j.Identifier).get(0).node.name;
+
+  modelImports.remove();
+
+  const modelInstances = root.find(j.CallExpression, {callee: {name: modelImportName }});
+  const objectExpression = modelInstances.find(j.ObjectExpression);
+
+  modelInstances.replaceWith(objectExpression.get(0).node);
+
+  return root.toSource();
+}
